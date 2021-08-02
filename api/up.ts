973c8f5ba7f4b3a288f188de231cdee18677c003
Elliana May <me@mause.me>
2021-08-02 13:12:42 +0800

    Add types

--- api/up.ts
+++ api/up.ts
@@ -1,6 +1,7 @@
 import Axios from "axios";
+import { VercelRequest, VercelResponse } from "@vercel/node";
 
-export default async function (req, res) {
+export default async (request: VercelRequest, response: VercelResponse) => {
   res.json(
     await Axios.get("https://api.up.com.au/api/v1/util/ping", {
       headers: { Authorization: "Bearer " + process.env.UP_TOKEN },
