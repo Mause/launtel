47e52655e3582c0ab7d3a3ebbb595e981acfd4be
Elliana May <me@mause.me>
2021-08-02 13:37:33 +0800

    Filter transactions

--- api/up.ts
+++ api/up.ts
@@ -1,12 +1,24 @@
 import Axios from "axios";
 import { VercelRequest, VercelResponse } from "@vercel/node";
 
+interface Amount {
+  valueInBaseUnits: number;
+}
+
+interface Transaction {
+  attributes: {
+    amount: Amount
+  }
+}
+
 export default async (request: VercelRequest, response: VercelResponse) => {
-  response.json(
-    (
-      await Axios.get<{}>("https://api.up.com.au/api/v1/util/ping", {
+  let transactions = (
+      await Axios.get<{data: Transaction[]}>("https://api.up.com.au/api/v1/transactions", {
         headers: { Authorization: "Bearer " + process.env.UP_TOKEN },
       })
-    ).data
+    ).data.data;
+
+  response.json(
+    transactions.filter(trans => trans.attributes.amount.valueInBaseUnits > 0)    
   );
 };
