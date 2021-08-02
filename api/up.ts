53e6b33be1ef3e11dde356ed41b48ad39bc44706
Elliana May <me@mause.me>
2021-08-02 13:15:14 +0800

    Correct variable name

--- api/up.ts
+++ api/up.ts
@@ -2,7 +2,7 @@ import Axios from "axios";
 import { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default async (request: VercelRequest, response: VercelResponse) => {
-  res.json(
+  response.json(
     await Axios.get("https://api.up.com.au/api/v1/util/ping", {
       headers: { Authorization: "Bearer " + process.env.UP_TOKEN },
     })
