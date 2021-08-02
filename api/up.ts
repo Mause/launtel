48390ce7918a88fbbfe86bafbeceecf7760a75b4
Elliana May <me@mause.me>
2021-08-02 13:11:37 +0800

    Fix syntax

--- api/up.ts
+++ api/up.ts
@@ -1,5 +1,5 @@
 import Axios from 'axios';
 
 export default async function(req, res) {
-  res.json(await Axios.get("https://api.up.com.au/api/v1/util/ping", {headers: {Authorization: "Bearer " + process.env.UP_TOKEN));
+  res.json(await Axios.get("https://api.up.com.au/api/v1/util/ping", {headers: {Authorization: "Bearer " + process.env.UP_TOKEN}}));
 }
