17d2d224a34b1983be6afa327981f2dde6b3e31c
Elliana May <me@mause.me>
2021-08-05 13:53:21 +0800

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/loading/i);
+  const linkElement = screen.getAllByText(/loading/i);
   expect(linkElement).toBeInTheDocument();
 });
