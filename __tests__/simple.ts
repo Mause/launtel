36ddd2a956c4cbe09301eeb4387f3dc822ac56d8
Elliana May <me@mause.me>
2021-09-01 06:47:47 +0000

    Fully implement one test

--- __tests__/simple.ts
+++ __tests__/simple.ts
@@ -1,8 +1,9 @@
 import { createServer } from "vercel-node-server";
 import listen from "test-listen";
 import axios from "axios";
-import routeUnderTest from "../api/usage";
+import routeUnderTest, { axios as axiosUp } from "../api/up";
 import { Server } from "net";
+import moxios from "moxios";
 
 let server: Server;
 let url: string;
@@ -10,13 +11,32 @@ let url: string;
 beforeAll(async () => {
   server = createServer(routeUnderTest);
   url = await listen(server);
+  moxios.install(axiosUp);
 });
 
 afterAll(() => {
   server.close();
+  moxios.uninstall(axiosUp);
 });
 
 it("should return the expected response", async () => {
-  const response = await axios.get(url, { params: { name: "Pearl" } });
-  expect(response.data).toBe("Hello Pearl");
+  moxios.stubOnce("GET", /.*/, {
+    status: 200,
+    response: {
+      data: [
+        {
+          attributes: {
+            amount: { valueInBaseUnits: 1500 },
+            rawText: "Internet bill",
+          },
+        },
+      ],
+      links: {
+        next: "https://up/next",
+      },
+    },
+  });
+  const response = await axios.get(url);
+  expect(response.status).toBe(200);
+  expect(response.data).toMatchSnapshot();
 });
