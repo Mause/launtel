f719413b399d4a3c2216bcdbc7d50ef8069c9174
Elliana May <me@mause.me>
2021-09-06 17:53:41 +0800

    Create openapi.yaml.ts

---
+++ api/openapi.yaml.ts
@@ -0,0 +1,6 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import { readFileSync } from 'fs';
+
+export default async (request: VercelRequest, response: VercelResponse) => {
+  response.status(200).json(readFileSync(__dirname + '/openapi.yaml'));
+};
