df92297b1e83f610e6cc3aa20e47e4b1359f24fd
Elliana May <me@mause.me>
2021-09-07 14:33:53 +0800

    Add model

--- api/usage.ts
+++ api/usage.ts
@@ -1,6 +1,11 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import { getCookie as getSession } from "./transactions";
 import authenticate from "../support/auth";
+import { IsNotEmpty } from "class-validator";
+
+class UsageResponse {
+  constructor(@IsNotEmpty() private usage: Record<string, string>) {}
+}
 
 export default authenticate(
   async (request: VercelRequest, response: VercelResponse) => {
