5fa903473de6f217d4c9e38386e636cbc2ac324f
Elliana May <me@mause.me>
2021-09-07 17:12:37 +0800

    Add shape to up.ts

--- api/up.ts
+++ api/up.ts
@@ -1,6 +1,16 @@
 import Axios from "axios";
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import authenticate from "../support/auth";
+import { IsNotEmpty, Validate } from 'class-validator';
+
+@Validate()
+class UpTransaction {}
+
+@Validate()
+class UpTransactionResponse {
+  constructor(public items: UpTransaction[]) {}
+}
+export const responseShape = UpTransactionResponse.name;
 
 interface Amount {
   valueInBaseUnits: number;
@@ -43,10 +53,12 @@ export default authenticate(
 
     response.status(200);
     response.json(
-      transactions.filter(
-        (trans) =>
-          trans.attributes.amount.valueInBaseUnits > 0 &&
-          trans.attributes.rawText
+      new UpTransactionResponse(
+        transactions.filter(
+          (trans) =>
+            trans.attributes.amount.valueInBaseUnits > 0 &&
+            trans.attributes.rawText
+        )
       )
     );
   }
