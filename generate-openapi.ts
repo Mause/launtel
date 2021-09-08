3c9c5319f79b0cdb1c546c178f8c2fccdf0f1756
Elliana May <me@mause.me>
2021-09-08 10:14:30 +0800

    Validate ref existence

--- generate-openapi.ts
+++ generate-openapi.ts
@@ -49,14 +49,23 @@ async function generateOpenapi() {
     }
   }
 
-  doc.setIn(
-    ["components", "schemas"],
-    doc.createNode(
-      validationMetadatasToSchemas({
-        refPointerPrefix: "#/components/schemas/",
-      })
-    )
-  );
+  const schemas = validationMetadatasToSchemas({
+    refPointerPrefix: "#/components/schemas/",
+  });
+  doc.setIn(["components", "schemas"], doc.createNode(schemas));
+  
+  for (const operation of (doc.get("paths") as YAMLMap<string,YAMLMap<string,string>>).items) {
+    if (operation.value) {
+      for (const verb of operation.value.items) {
+        let ref = verb.value.getIn(['responses', 'default', 'content', 'application/json', 'schema', '$ref']) as string;
+        const parts = ref.split('/');
+        ref = parts[parts.length-1];
+        if (!schemas[ref]) {
+          throw new Error(`Couldn't find ${ref}`);
+        }
+      }
+    }
+  }
 
   doc.setIn(
     ["components", "schemas", "DummyResponse"],
